73e699bfe8d139d0786169a8961d0a4f16ea773e
Gabrielle Despaigne <Gdespaigne@yahoo.com>
2025-11-14 21:22:47 -0800

    lab7

--- map.js
+++ map.js
@@ -3,3 +3,15 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2Rlc3BhaWduZS11Y3NkIiwiYSI6ImNtaHpzbzB6bDB0MDQyam9pejdmYWVlN2cifQ.3kCscqIr4QQa4P6cYKncgg';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/gdespaigne-ucsd/cmhztb5i800kk01sua5s53upz',
+    center: [-71.09415, 42.36027],
+    zoom: 12,
+    minZoom: 5,
+    maxZoom: 18
+  });
+  
